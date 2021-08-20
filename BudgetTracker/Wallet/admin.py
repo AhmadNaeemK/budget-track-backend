7c9e8fa877ee80798c76fcfd1dc1c04860f9c193
Ahmad Naeem <mnaeem.bese17seecs@seecs.edu.pk>
2021-08-20 18:31:55 +0500

    Created Transaction and wallet model Features and initiated react frontend

--- BudgetTracker/Wallet/admin.py
+++ BudgetTracker/Wallet/admin.py
@@ -1,29 +1,33 @@
 from django.contrib import admin
 
-
-from .models import Transactions, Accounts, Wallet
-# Register your models here.
+from .models import Transaction, Account, Wallet
 
 
 class TransactionAdminInline(admin.TabularInline):
-    model = Transactions
+    model = Transaction
+    fk_name = 'debit_account'
     extra = 2
 
 
 class AccountsAdminInline(admin.TabularInline):
-    model = Accounts
+    model = Account
     extra = 1
 
 
 class WalletAdmin(admin.ModelAdmin):
     inlines = [AccountsAdminInline]
-    list_display = ('user', 'start_tracking_date', 'calc_balance')
+    list_display = ('user', 'get_balance', 'get_cash_balance', 'start_tracking_date')
 
 
 class AccountsAdmin(admin.ModelAdmin):
     inlines = [TransactionAdminInline]
-    list_display = ('title', 'wallet', 'category', 'amount')
+    list_display = ('title', 'get_wallet_id', 'get_user', 'category')
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('title', 'get_credit_account', 'get_debit_account', 'transaction_date', 'amount')
 
 
 admin.site.register(Wallet, WalletAdmin)
-admin.site.register(Accounts, AccountsAdmin)
+admin.site.register(Account, AccountsAdmin)
+admin.site.register(Transaction, TransactionAdmin)
--- BudgetTracker/Wallet/models.py
+++ BudgetTracker/Wallet/models.py
@@ -4,19 +4,38 @@ from django.contrib import admin
 
 
 class Wallet(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
     start_tracking_date = models.DateField()
-    gross_income = models.FloatField()
-    gross_expenses = models.FloatField()
+
+    def save(self, *args, **kwargs):
+        # on creation of wallet
+        if not self.pk:
+            cash = Account(wallet=self, title='Cash', category='Cash')
+            super(Wallet, self).save(*args, **kwargs)
+            cash.save()
 
     @admin.display(
         description='Balance'
     )
-    def calc_balance(self):
-        return self.gross_income - self.gross_expenses
+    def get_balance(self):
+        accounts = Account.objects.filter(wallet=self)
+        balance = 0
+        for account in accounts:
+            balance += account.get_balance()
+        return balance
+
+    @admin.display(description='Remaining Cash')
+    def get_cash_balance(self):
+        cash_accounts = Account.objects.filter(wallet=self, category='Cash')
+        remaining_cash = 0
+        for account in cash_accounts:
+            remaining_cash += account.get_balance()
+        if remaining_cash < 0:
+            print('You have negative cash flow, ', remaining_cash)
+        return remaining_cash
 
 
-class Accounts(models.Model):
+class Account(models.Model):
     wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
 
@@ -24,27 +43,75 @@ class Accounts(models.Model):
         ('HealthCare', 'Doctor/Medicine'),
         ('Loans/Interests', 'Loans/Interests'),
         ('Travel', 'Petrol/Transport'),
-        ('Cash', 'Money')
+        ('Cash', 'Cash/Bank/'),
+        ('Lifestyle', 'Grocery/Sports'),
+        ('Food', 'Restaurant/Fast Food'),
+        ('Salary', 'Salary/Profit/Income'),
+        ('Other', 'Other')
         ]
-
     category = models.TextField(choices=account_category_choices)
-    amount = models.FloatField()
+
+    debit_amount = models.FloatField(default=0)
+    credit_amount = models.FloatField(default=0)
+
+    def get_debit(self):
+        d_amount = 0
+        transactions = Transaction.objects.filter(debit_account=self)
+        for transaction in transactions:
+            d_amount += transaction.amount
+        return d_amount
+
+    def get_credit(self):
+        c_amount = 0
+        transactions = Transaction.objects.filter(credit_account=self)
+        for transaction in transactions:
+            c_amount += transaction.amount
+        return c_amount
+
+    def get_balance(self):
+        self.debit_amount = self.get_debit()
+        self.credit_amount = self.get_credit()
+        return self.get_debit() - self.get_credit()
+
+    def update_account(self):
+        self.credit_amount = self.get_credit()
+        self.debit_amount = self.get_debit()
+        self.save()
+
+    @admin.display(description='User')
+    def get_user(self):
+        return self.wallet.user
+
+    @admin.display(description='Wallet_id')
+    def get_wallet_id(self):
+        return self.wallet.id
+
+    @admin.display(description='Balance')
+    def get_balance(self):
+        return self.debit_amount - self.credit_amount
+
+    class Meta:
+        unique_together = [['wallet', 'title']]
 
 
-class Transactions(models.Model):
-    account = models.ForeignKey(Accounts, on_delete=models.CASCADE)
+class Transaction(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    debit_account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='debit_account')
+    credit_account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='credit_account')
+    transaction_date = models.DateTimeField(auto_now=True)
     amount = models.FloatField()
-    debit = models.BooleanField()
 
     def save(self, *args, **kwargs):
-        if self.debit:
-            self.account.wallet.gross_income += self.amount
-            self.account.amount += self.amount
-        else:
-            self.account.wallet.gross_expenses += self.amount
-            self.account.amount -= self.amount
+        super(Transaction, self).save(*args, **kwargs)
+        self.debit_account.update_account()
+        self.credit_account.update_account()
 
-        self.account.save()
-        self.account.wallet.save()
-        super(Transactions, self).save(*args, **kwargs)
+    @admin.display(description='Credit Account')
+    def get_credit_account(self):
+        return self.credit_account.title
+
+    @admin.display(description='Debit Account')
+    def get_debit_account(self):
+        return self.debit_account.title
 
