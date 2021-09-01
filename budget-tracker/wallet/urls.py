c00e17a828633bfb2ff2f7ca1befb2aa2b4358e6
Ahmad Naeem <mnaeem.bese17seecs@seecs.edu.pk>
2021-09-01 13:48:02 +0500

    Added Accounts Crud

--- budget-tracker/wallet/urls.py
+++ budget-tracker/wallet/urls.py
@@ -5,4 +5,5 @@ app_name = 'wallet'
 urlpatterns = [
     path('transactionlist/', views.TransactionList.as_view(), name='transaction_list'),
     path('accountsList/', views.AccountList.as_view(), name='accounts_list'),
+    path('accountsCategoryList/', views.AccountCategoryChoicesList.as_view(), name='category_list'),
 ]
