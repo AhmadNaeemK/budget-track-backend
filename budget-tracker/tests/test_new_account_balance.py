ce1ae4ad06ac5bbbc8c54cdc2ea80089d523ad24
Ahmad Naeem <mnaeem.bese17seecs@seecs.edu.pk>
2021-11-12 10:38:54 +0500

    Add: Tests for account balance update
    Tests for account balance calculation

---
+++ budget-tracker/tests/test_new_account_balance.py
@@ -0,0 +1,15 @@
+import pytest
+
+from wallet.utils import TransactionUtils
+from wallet.models import TransactionCategories
+
+test_data = [
+    (0, 20, TransactionCategories.Income.value, 20),
+    (20, 20, TransactionCategories.Income.value, 40),
+    (60, 10, TransactionCategories.Drink.value, 50)
+]
+
+
+@pytest.mark.parametrize('prev_balance,amount,type,expected', test_data)
+def test_new_account_balance(prev_balance, amount, type, expected):
+    assert TransactionUtils.get_new_account_balance(prev_balance, amount, type) == expected
